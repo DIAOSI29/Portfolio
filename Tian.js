da22bed2cc33a44209f4fb148900b30ccc861551
Tian Qin <qintian29@gmail.com>
2019-11-17 12:44:37 +0800

    landing page setup

--- Tian.js
+++ Tian.js
@@ -14,17 +14,17 @@ let skillset = [
   "GIT"
 ];
 
-for (let skill of skillset) {
-  let eachA = $("<a>");
-  let eachLi = $("<li>");
-  eachA.html(skill);
-  eachA.addClass("landing-page-button");
-  eachA.attr("href", skill);
-  eachLi.addClass("col-3 ol-sm-3 col-md-2 col-lg-1");
-  eachLi.append(eachA);
-  $("#buttons").append(eachLi);
+function generateLandingPageButton() {
+  for (let skill of skillset) {
+    let eachA = $("<a>");
+    let eachLi = $("<li>");
+    eachA.text(skill);
+    eachA.addClass("landing-page-button");
+    eachA.attr("href", skill);
+    eachLi.addClass("optionButton col-3 ol-sm-3 col-md-2 col-lg-1");
+    eachLi.append(eachA);
+    $("#buttons").append(eachLi);
+  }
 }
 
-$("#buttons").css("width", 0);
-
-document.alert("hello");
+generateLandingPageButton();
