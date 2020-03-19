a214c02232ad226e974a1f1e1a302a08ec18207b
Tian Qin <qintian29@gmail.com>
2020-03-19 15:10:41 +0800

    deploy

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ var express = require("express");
 var PORT = process.env.PORT || 1111;
 var app = express();
 
-app.use(express.static("index"));
+app.use(express.static("static"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.send("./index.html");
+// });
 
 app.listen(PORT, function() {
   console.log("Listening on port: ", PORT);
